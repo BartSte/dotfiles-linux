fb1a19da5ec9b6f4df47893608fa7b5e43774ee1
barts <bartsteensma@outlook.com>
2022-05-04 19:23:32 +0200

    install fzf from source is added

--- dotfiles-linux/install_vim_from_source.sh
+++ dotfiles-linux/install_vim_from_source.sh
@@ -1,7 +1,7 @@
 
 sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
  
-sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
+sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev python3-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
 
 #Optional: so vim can be uninstalled again via `dpkg -r vim`
 sudo apt-get install checkinstall
@@ -26,7 +26,7 @@ cd ..
  --enable-pythoninterp=dynamic \
  --with-python-config-dir=/usr/lib/python2.7/config-arm-linux-gnueabihf \
  --enable-python3interp \
- --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-arm-linux-gnueabihf \
+ --with-python3-config-dir=/usr/lib/python3.9/config-3.9-aarch64-linux-gnu \
  --enable-luainterp \
  --with-luajit \
  --enable-cscope \
