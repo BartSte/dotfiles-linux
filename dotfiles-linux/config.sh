c21e6f7cd9dc1788e80017ac7bc13c51731554c0
barts <bartsteensma@outlook.com>
2022-06-10 13:05:32 +0000

    added gruvbox to i3 config

--- dotfiles-linux/config.sh
+++ dotfiles-linux/config.sh
@@ -1,3 +1,3 @@
-dependencies=("python3.10" "python3-pip" "silversearcher-ag" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd-find")
+dependencies=("feh" "python3.10" "python3-pip" "silversearcher-ag" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd-find")
 
 dependencies_extra=("xorg" "dwm" "picom" "dmenu" "nitrogen" "i3blocks")
