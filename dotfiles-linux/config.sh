3c2b0eb738e79595802f3f4bcedd2109dadf99f4
barts <bartsteensma@outlook.com>
2022-06-18 08:32:05 +0000

    fix picom

--- dotfiles-linux/config.sh
+++ dotfiles-linux/config.sh
@@ -1,3 +1,3 @@
 dependencies=("xev" "urlview" "gnupg-agent" "unzip" "feh" "python3.10" "python3-pip" "silversearcher-ag" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd-find")
 
-dependencies_extra=("xbacklight" "pulseaudio" "scrot" "polybar" "fonts-firacode" "compton" "rofi" "fonts-font-awesome" "xorg" "i3blocks")
+dependencies_extra=("xbacklight" "pulseaudio" "scrot" "polybar" "fonts-firacode" "picom" "rofi" "fonts-font-awesome" "xorg" "i3blocks")
