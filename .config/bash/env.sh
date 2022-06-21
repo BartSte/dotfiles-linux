1d3599104e76ab544111959b3b6e0e2f84b52cb7
barts <b.steensma@fleetcleaner.com>
2022-06-21 18:13:00 +0200

    Gruvbox prompt

--- .config/bash/env.sh
+++ .config/bash/env.sh
@@ -1,7 +1,6 @@
 export PROMPT_COMMAND='echo -en "\033]0;$(hostname) @ $(pwd)\a"'
 export PS0="\e[2 q"
-# export PS1="\\$ \[$(tput sgr0)\]\[\033[38;5;10m\]\W\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;6m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')\[$(tput sgr0)\]>\[$(tput sgr0)\]"
-export PS1="\\$ \[$(tput sgr0)\]\[\033[38;5;10m\]\W\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')\[$(tput sgr0)\]>\[$(tput sgr0)\]"
+export PS1="\\$ \[$(tput sgr0)\]\[\033[38;5;14m\]\W\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;10m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')\[$(tput sgr0)\]>\[$(tput sgr0)\]"
 
 export GPG_TTY=$(tty)
 export EDITOR='vim'
