6d0a566151eff42fe9e8cf1a41c5e261800f225c
barts <b.steensma@fleetcleaner.com>
2022-07-08 11:20:25 +0200

    bugfix firefox_win

--- dotfiles-linux/vim/install_vim_from_source.sh
+++ dotfiles-linux/vim/install_vim_from_source.sh
@@ -1,10 +1,5 @@
-
 sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
- 
-sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev python3-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
-
-#Optional: so vim can be uninstalled again via `dpkg -r vim`
-sudo apt-get install checkinstall
+sudo apt-get install checkinstall liblua5.1-dev luajit libluajit-5.1 python2-dev python3-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
 
 sudo rm -rf /usr/local/share/vim /usr/bin/vim
 
@@ -12,8 +7,6 @@ cd ~
 git clone https://github.com/vim/vim
 cd vim
 git pull && git fetch
-
-#In case Vim was already installed
 cd src
 make distclean
 cd ..
@@ -24,9 +17,9 @@ cd ..
  --enable-rubyinterp=dynamic \
  --with-ruby-command=/usr/bin/ruby \
  --enable-pythoninterp=dynamic \
- --with-python-config-dir=/usr/lib/python2.7/config-arm-linux-gnueabihf \
+ --with-python-config-dir=/usr/lib/python2.7/plat-x86_64-linux-gnu \
  --enable-python3interp \
- --with-python3-config-dir=/usr/lib/python3.9/config-3.9-aarch64-linux-gnu \
+ --with-python3-config-dir=/usr/lib/python3.10/config-3.10-x86_64-linux-gnu \
  --enable-luainterp \
  --with-luajit \
  --enable-cscope \
@@ -36,7 +29,7 @@ cd ..
  --enable-fontset \
  --enable-largefile \
  --disable-netbeans \
- --with-compiledby="yourname" \
+ --with-compiledby="barts" \
  --enable-fail-if-missing
 
 make && sudo make install
