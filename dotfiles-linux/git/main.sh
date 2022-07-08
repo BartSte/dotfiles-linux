6d570919fdbd59a2c16ac928def12b82b4454365
barts <b.steensma@fleetcleaner.com>
2022-07-08 16:09:55 +0200

    moved mutt files to dotfiles-linux folder

--- dotfiles-linux/git/main.sh
+++ dotfiles-linux/git/main.sh
@@ -6,7 +6,6 @@ function init_git() {
     git config --global core.autocrlf input
     git config --global user.name $GIT_NAME
     git config --global user.email "$MUTT_USERNAME@$MUTT_DOMAIN"
-    git config --global status.showUntrackedFiles no
 
     batcat ~/.gitconfig
 }
