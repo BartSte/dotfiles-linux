be50d7736046bf285cfffd9ecc8354f7fb49cb2a
barts <bartsteensma@outlook.com>
2022-07-17 21:18:16 +0200

    add custom scripts

--- dotfiles-linux/dependencies/deps.sh
+++ dotfiles-linux/dependencies/deps.sh
@@ -1,4 +1,4 @@
-dependencies=("markdown" "software-properties-common" "exa" "trash-cli" "urlview" "gnupg-agent" "unzip" "feh" "python3.10" "python3-pip" "silversearcher-ag" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd-find" "poppler-utils" "antiword" "odt2txt" "tar" "p7zip-full" "unzip" "fuse" "libfuse2")
+dependencies=("python-is-python3" "markdown" "software-properties-common" "exa" "trash-cli" "urlview" "gnupg-agent" "unzip" "feh" "python3.10" "python3-pip" "silversearcher-ag" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd-find" "poppler-utils" "antiword" "odt2txt" "tar" "p7zip-full" "unzip" "fuse" "libfuse2")
 dependencies_snap=("firefox" "whatsapp-4linux" "spotify")
 dependencies_extra=("pulseaudio-module-bluetooth" "pavucontrol" "xbacklight" "pulseaudio" "scrot" "polybar" "picom" "rofi" "xorg")
 
