4483919272b52d823253c01054dd273863ccae3e
barts <bartsteensma@outlook.com>
2022-07-27 22:27:05 +0200

    fixed intel driver

--- dotfiles-linux/asus/main_asus.sh
+++ dotfiles-linux/asus/main_asus.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# The following function are specifically for the asus vivobook running ubuntu 22
+# The following function are specifically for the asus vivobook 
 
 function copy_backlight_config() {
     sudo cp ~/dotfiles-linux/asus/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf --no-clobber
 }
 
 copy_backlight_config
+sudo pacman -S xf86-video-intel
