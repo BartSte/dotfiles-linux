ea2a49296f216f7033b66bbf2a3f2201d039e1fd
barts <bartsteensma@outlook.com>
2022-07-27 21:41:10 +0200

    bugfix bluetooth

--- dotfiles-linux/i3/main.sh
+++ dotfiles-linux/i3/main.sh
@@ -2,11 +2,11 @@
 . ~/dotfiles-linux/dependencies/helpers.sh
 
 function install_polybar_collections() {
-    echo "Clony BartSte/polybar-collection"
+    echo "Clone BartSte/polybar-collection"
     git clone https://github.com/BartSte/polybar-collection ~/polybar-collection
     git switch develop
     git pull
-    cp ~/polybar-collection/fonts ~/.fonts
+    cp ~/polybar-collection/fonts ~/.fonts -r
     cd ~/.fonts
     fc-cache -fv
     cd ~
@@ -23,6 +23,4 @@ echo "# I3 window manager"
 install_polybar_collections 
 set_time_zone $TIME_ZONE
 install_dependencies "${dependencies_extra[@]}"
-for package in "${dependencies_snap[@]}"; do
-    sudo snap install $package
-done
+systemctl start bluetooth
