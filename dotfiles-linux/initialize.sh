2df79da556a97928a713799ae9987f01a5d25997
barts <bartsteensma@outlook.com>
2022-08-02 16:31:00 +0200

    update initialize

--- dotfiles-linux/initialize.sh
+++ dotfiles-linux/initialize.sh
@@ -3,7 +3,7 @@ function clone() {
 }
 
 function checkout() {
-    git --work-tree=$HOME --git-dir=$1 checkout
+    git --work-tree=$HOME --git-dir=$1 checkout -f
 }
 
 github="https://github.com/BartSte" 
@@ -14,19 +14,27 @@ base_dir="$HOME/dotfiles.git"
 lin="$github/dotfiles-linux.git"
 lin_dir="$HOME/dotfiles-linux.git"
 
+sec="$github/dotfiles-secret.git"
+sec_dir="$HOME/dotfiles-secret.git"
+
 echo "# Installing git"
-sudo  pacman -S git
+sudo  pacman -S git exa inetutils
 
 echo "# Clone BartSte/dotfiles.git as a bare repository"
 clone $base $base_dir 
 checkout $base_dir
-rm "$HOME/.gitignore"
-rm "$HOME/README.md"
 
 echo "# Clone BartSte/dotfiles-linux.git as a bare repository"
 clone $lin $lin_dir 
 checkout $lin_dir
+
+echo "# If you have permission: clone BartSte/dotfiles-secret.git as a bare repository"
+clone $sec $sec_dir 
+checkout $sec_dir
+
+echo "# If you have permission: clone BartSte/dotfiles-secret.git as a bare repository"
 source ~/.bashrc
+bash ~/dotfiles-linux/wsl/main.sh
 
 echo "# Please complete the file ~/dotfiles-linux/config.sh"
 echo "# Next, run the ~/dotfiles-linux/main.sh"
