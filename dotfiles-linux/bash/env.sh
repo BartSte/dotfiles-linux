92ef917ba186ec9c415f87c8653d09a477614a26
barts <bartsteensma@outlook.com>
2022-08-02 17:01:34 +0200

    finetune polybar

--- dotfiles-linux/bash/env.sh
+++ dotfiles-linux/bash/env.sh
@@ -1,5 +1,4 @@
-#TODO fzf is not loaded correctly. .fzf.bash does net exist so bash cannot find fzf.
-export PROMPT_COMMAND='echo -en "\033]0;$(hostname)"'
+export PROMPT_COMMAND='echo -en "\033]0;$(hostname) @ $(pwd)"'
 export PS0="\e[2 q"
 export PS1="\\$ \[$(tput sgr0)\]\[\033[38;5;14m\]\W\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;10m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')\[$(tput sgr0)\]>\[$(tput sgr0)\]"
 
