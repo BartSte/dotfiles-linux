bdb94ffd17ebe2f5813b71ce6a1f2c25257fa90f
barts <b.steensma@fleetcleaner.com>
2022-08-02 13:42:08 +0200

    Added automation for oauth2 in main script

--- dotfiles-linux/main.sh
+++ dotfiles-linux/main.sh
@@ -5,6 +5,7 @@ bash ~/dotfiles-linux/dependencies/main.sh
 bash ~/dotfiles-linux/wsl/main.sh
 bash ~/dotfiles-linux/fonts/main.sh
 bash ~/dotfiles-linux/vim/main.sh
+bash ~/dotfiles-linux/gpg/main.sh
 bash ~/dotfiles-linux/mutt/main.sh
 bash ~/dotfiles-linux/i3/main.sh
 
