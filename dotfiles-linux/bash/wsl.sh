08625053c29bad6132d6af7fd338e39499b6b038
barts <b.steensma@fleetcleaner.com>
2022-08-08 17:38:28 +0200

    update vimspector

--- dotfiles-linux/bash/wsl.sh
+++ dotfiles-linux/bash/wsl.sh
@@ -7,7 +7,7 @@ function winenv() {
 
 export WH=$(wslpath "$(winenv USERPROFILE)")
 alias wh='cd $WH'
-alias ps="powershell.exe -NoExit -Command 'cls'"
-alias psh="powershell.exe -NoExit -Command 'cd ~'"
+alias pow="powershell.exe -NoExit -Command 'cls'"
+alias powh="powershell.exe -NoExit -Command 'cd ~'"
 alias ex="Explorer.exe ."
 
