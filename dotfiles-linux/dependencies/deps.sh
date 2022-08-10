de9e7cbf4fe06f15380298f9bf774c5b0e367a32
barts <bartsteensma@outlook.com>
2022-08-10 11:24:28 +0200

    Enable kmonad on buildin keyboard

--- dotfiles-linux/dependencies/deps.sh
+++ dotfiles-linux/dependencies/deps.sh
@@ -1,4 +1,4 @@
-dependencies=("fzf" "python" "python-markdown" "powerline-fonts" "exa" "trash-cli" "unzip" "feh" "python-pip" "the_silver_searcher" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd" "poppler" "antiword" "odt2txt" "tar" "p7zip" "fuse2" "fuse3" "fuse-common" "inetutils" "redshift" "base-devel" "neofetch" "ccls")
-dependencies_aur=("urlview")
+dependencies=("fzf" "python" "python-markdown" "powerline-fonts" "exa" "trash-cli" "unzip" "feh" "python-pip" "the_silver_searcher" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd" "poppler" "antiword" "odt2txt" "tar" "p7zip" "fuse2" "fuse3" "fuse-common" "inetutils" "redshift" "base-devel" "neofetch" "ccls" "gdb")
+dependencies_aur=("urlview" "kmonad-bin")
 dependencies_extra=("firefox" "networkmanager" "rofi" "pavucontrol" "pulseaudio-alsa" "alsa-utils" "pulseaudio-bluetooth" "bluez-utils" "pulseaudio" "xorg" "xorg-xbacklight" "scrot" "polybar" "picom" "i3-wm")
 
