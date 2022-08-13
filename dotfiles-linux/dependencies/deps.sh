4813f6af3a7feb4952df2514f905ea2640e63f94
barts <bartsteensma@outlook.com>
2022-08-13 09:52:49 +0200

    Add cpp unittest

--- dotfiles-linux/dependencies/deps.sh
+++ dotfiles-linux/dependencies/deps.sh
@@ -1,4 +1,4 @@
-dependencies=("fzf" "python" "python-markdown" "powerline-fonts" "exa" "trash-cli" "unzip" "feh" "python-pip" "the_silver_searcher" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd" "poppler" "antiword" "odt2txt" "tar" "p7zip" "fuse2" "fuse3" "fuse-common" "inetutils" "redshift" "base-devel" "neofetch" "ccls" "gdb")
+dependencies=("fzf" "python" "python-markdown" "powerline-fonts" "exa" "trash-cli" "unzip" "feh" "python-pip" "the_silver_searcher" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd" "poppler" "antiword" "odt2txt" "tar" "p7zip" "fuse2" "fuse3" "fuse-common" "inetutils" "redshift" "base-devel" "neofetch" "ccls" "gdb" "make" "cmake")
 dependencies_aur=("urlview" "kmonad-bin")
 dependencies_extra=("firefox" "networkmanager" "rofi" "pavucontrol" "pulseaudio-alsa" "alsa-utils" "pulseaudio-bluetooth" "bluez-utils" "pulseaudio" "xorg" "xorg-xbacklight" "scrot" "polybar" "picom" "i3-wm")
 
