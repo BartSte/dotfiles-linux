d2d6ad54d8aaddbe0dedaa3c9c4beb347f12c261
barts <b.steensma@fleetcleaner.com>
2022-08-23 09:24:36 +0200

    Added full page view to fzf_help

--- dotfiles-linux/scripts/fzf_help.bash
+++ dotfiles-linux/scripts/fzf_help.bash
@@ -4,27 +4,39 @@ _make_fzf_help_regex() {
     _obligatory_chars='--'
     _allowed_symbols='\[\]\-\=\.\,\%'
     _allowed_letters_and_numbers='a-zA-Z0-9'
+
     _FZF_HELP_REGEX="($_regex_head)($_obligatory_chars)"
     _FZF_HELP_REGEX+="([$_allowed_letters_and_numbers]+"
     _FZF_HELP_REGEX+="[$_allowed_letters_and_numbers$_allowed_symbols]*)"
     _FZF_HELP_REGEX+="($_regex_tail)"
+
     export _FZF_HELP_REGEX
 }
 
 _make_fzf_help_opts() {
     _get_line_number='number=$(echo "$_FZF_HELP_RESULTS" | ag -Q -- {} | head -1 | sed "s/:.*$//g");'
-    _get_scroll_number='half_page=$(($FZF_PREVIEW_LINES / 2));scroll=$(($number-$half_page));'
-    _highlight_line='$_FZF_HELP_COMMAND --help | bat -f -p -H $number --theme Dracula --line-range $(($scroll > 0 ? $scroll : 0)):500'
-    export _FZF_HELP_PREVIEW_OPTS="$_regex_line_number $_get_line_number $_get_scroll_number $_highlight_line"
-    export _FZF_HELP_OTHER_OPTS="--preview-window=right,75%" 
+    _get_line_number+='[ {q} == "##" ] && number=0;'
+
+    _get_scroll_number='half_page=$(($FZF_PREVIEW_LINES / 2));'
+    _get_scroll_number+='scroll=$(($number-$half_page));'
+    _get_scroll_number+='scroll=$(($scroll > 0 ? $scroll : 0));'
+
+    _write_to_stdout='$_FZF_HELP_COMMAND --help | bat -f -p -H $number -r $scroll: --theme Dracula;'
+
+    export _FZF_HELP_PREVIEW_OPTS="$_get_line_number $_get_scroll_number $_write_to_stdout"
+    export _FZF_HELP_OTHER_OPTS='--preview-window=right,75%' 
+    export _FZF_HELP_HEADER='Type ## for full page view'
 }
 
 _fzf_help() {
-    #TODO store the line numbers + search results of the first `ag` search. Use this later for highlighting and scrolling.
-    export _FZF_HELP_COMMAND=$(echo $READLINE_LINE | sed 's/\( -\).*$//')
+    regex_get_command='s/\( -\).*$//'
+    export _FZF_HELP_COMMAND=$(echo $READLINE_LINE | sed "$regex_get_command")
     builtin typeset READLINE_LINE_NEW=$(
-        export _FZF_HELP_RESULTS=$("$_FZF_HELP_COMMAND" --help | ag --numbers --only-matching -- "$_FZF_HELP_REGEX");
-        echo "$_FZF_HELP_RESULTS" | sed "s/^.*://g" | fzf "$_FZF_HELP_OTHER_OPTS" --preview "$_FZF_HELP_PREVIEW_OPTS"
+        regex_remove_line_number='s/^.*://g';
+        export _FZF_HELP_RESULTS=$("$_FZF_HELP_COMMAND" --help | 
+                                   ag -o --numbers -- "$_FZF_HELP_REGEX");
+        echo "$_FZF_HELP_RESULTS" | sed "$regex_remove_line_number" | 
+             fzf $_FZF_HELP_OTHER_OPTS --preview "$_FZF_HELP_PREVIEW_OPTS" --header="$_FZF_HELP_HEADER"
     )
     _write_line
 }
