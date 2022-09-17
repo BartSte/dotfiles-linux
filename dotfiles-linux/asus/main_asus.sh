fa7c7b8d3369148b071c9b0d206917c59fa108a5
barts <bartsteensma@outlook.com>
2022-09-17 20:46:02 +0200

    added wireless config

--- dotfiles-linux/asus/main_asus.sh
+++ dotfiles-linux/asus/main_asus.sh
@@ -6,5 +6,10 @@ function copy_backlight_config() {
     sudo cp ~/dotfiles-linux/asus/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf --no-clobber
 }
 
+function copy_wireless_adapter_config() {
+    sudo cp ~/dotfiles-linux/asus/main.conf /etc/iwd/main.conf
+}
+
 copy_backlight_config
+copy_wireless_adapter_config
 sudo pacman -S xf86-video-intel
