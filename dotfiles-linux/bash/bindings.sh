da6233c345ef9494081d2d7ef87dc3b3a57dd6ab
barts <b.steensma@fleetcleaner.com>
2022-09-27 09:14:52 +0200

    update for tarmak 2

--- dotfiles-linux/bash/bindings.sh
+++ dotfiles-linux/bash/bindings.sh
@@ -1,7 +1,10 @@
 set -o vi
-bind '"\C-f":vi-movement-mode'
+
+bind '"\C-t":vi-movement-mode'
 bind '"\C-h":"\C-w"'
 bind -x '"\t": fzf_bash_completion'
 bind -x '"\C-l": clear;'
 bind -x '"\C-a": fzf_help;'
+bind -x '"\eo": fzf-file-widget'
+bind -x '"\eo": fzf-file-widget'
 
