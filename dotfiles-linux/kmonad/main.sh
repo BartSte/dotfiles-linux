8ae67e64e77fb8b4b80e37177dba536fb82864c5
barts <bartsteensma@outlook.com>
2022-10-02 09:23:59 +0200

    added kmonad service at system level

---
+++ dotfiles-linux/kmonad/main.sh
@@ -0,0 +1,21 @@
+enable_service() {
+    sudo killall kmonad
+    sudo systemctl disable kmonad.service
+
+    pushd .
+    cd ~/dotfiles-linux/kmonad
+    sudo cp kmonad.service /etc/systemd/system
+    sudo cp home_row_modifiers.kbd /etc/systemd/system
+
+    cd /etc/systemd/system
+    sudo chmod 644 kmonad.service
+    sudo chmod 644 home_row_modifiers.kbd
+    sudo chown root kmonad.service
+    sudo chown root home_row_modifiers.kbd
+
+    popd
+    sudo systemctl start kmonad.service
+    sudo systemctl enable kmonad.service
+}
+
+enable_service
