86fe3bd29de6fef4a5b2df1411ef74653b080d4d
barts <bartsteensma@outlook.com>
2022-10-03 21:33:05 +0200

    added symlink func for init.vim

--- dotfiles-linux/vim/main.sh
+++ dotfiles-linux/vim/main.sh
@@ -5,5 +5,10 @@ function install_vim_plug () {
     vim -c PlugInstall\|qa!
 }
 
+sym_link_init_vim () {
+    ln ~/init.vim ~/.config/nvim/init.vim --symbolic
+}
+
 echo "# Vim"
 install_vim_plug
+sym_link_init_vim 
