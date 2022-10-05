d2b57ba48ea4bc55c12b63b43bb0cbdd0b135b8a
barts <b.steensma@fleetcleaner.com>
2022-10-05 20:46:52 +0200

    bugfix fzf

--- dotfiles-linux/bash/fzf.bash
+++ dotfiles-linux/bash/fzf.bash
@@ -3,6 +3,9 @@
 [ -f ~/dotfiles-linux/scripts/fzf_help.bash ] && source ~/dotfiles-linux/scripts/fzf_help.bash
 
 export FZF_DEFAULT_COMMAND="fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore --type f"
+export FZF_ALT_C_COMMAND="fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d"
+export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+
 export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --preview-window=right,65%"
 
 _FZF_CTRL_T_OPTS_FILES='bat --theme=gruvbox-dark --style=numbers --color=always --line-range :500 {}'
@@ -10,12 +13,10 @@ _FZF_CTRL_T_OPTS_DIR='exa --icons -T -L 1 -a {} | head -200'
 _FZF_CTRL_T_OPTS_BIND="ctrl-d:reload($FZF_ALT_C_COMMAND)+change-preview($_FZF_CTRL_T_OPTS_DIR)+toggle-preview,"
 _FZF_CTRL_T_OPTS_BIND+="ctrl-f:reload($FZF_CTRL_T_COMMAND)+change-preview($_FZF_CTRL_T_OPTS_FILES)+change-preview-window(hidden),"
 _FZF_CTRL_T_OPTS_BIND+="ctrl-s:toggle-preview"
-export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 export FZF_CTRL_T_OPTS="--bind '$_FZF_CTRL_T_OPTS_BIND' "
 export FZF_CTRL_T_OPTS+="--preview '$_FZF_CTRL_T_OPTS_FILES' "
 export FZF_CTRL_T_OPTS+="--preview-window 'hidden'"
 
-export FZF_ALT_C_COMMAND="fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d"
 export FZF_ALT_C_OPTS="--preview '$_FZF_CTRL_T_OPTS_DIR' "
 export FZF_ALT_C_OPTS+="--bind 'ctrl-s:toggle-preview' "
 export FZF_ALT_C_OPTS+="--preview-window hidden"
