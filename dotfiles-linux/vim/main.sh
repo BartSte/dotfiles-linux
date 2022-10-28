3ba787daf23f545f8b827761495db6e9e99a1612
barts <bartsteensma@outlook.com>
2022-10-28 21:34:41 +0200

    moved to packer

--- dotfiles-linux/vim/main.sh
+++ dotfiles-linux/vim/main.sh
@@ -1,17 +1,3 @@
-function install_vim_plug () {
-    echo "Instal vim plug"
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    vim -c PlugInstall\|qa!
-}
-
-function install_nvim_plug () {
-    echo "Instal nvim plug"
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    nvim -c PlugInstall\|qa!
-}
-
 sym_link_init_vim () {
     mkdir ~/.config/nvim
     rm ~/.config/nvim/init.vim
@@ -20,5 +6,4 @@ sym_link_init_vim () {
 
 echo "# Vim"
 sym_link_init_vim 
-install_nvim_plug
 pip install pynvim
