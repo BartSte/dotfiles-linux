f27a6c1fc6df8ee8edec39c034246e54d1219fe8
barts <bartsteensma@outlook.com>
2022-10-29 18:20:47 +0200

    nvim removed vim folder and added lua folder

--- dotfiles-linux/vim/main.sh
+++ dotfiles-linux/vim/main.sh
@@ -2,11 +2,11 @@ symlink_config () {
     mkdir ~/.config/nvim
     rm ~/.config/nvim/init.vim
     rm ~/.config/nvim/after -r
-    rm ~/.config/nvim/vim -r
+    rm ~/.config/nvim/lua -r
 
     ln ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim --symbolic
     ln ~/dotfiles/vim/after ~/.config/nvim/after --symbolic
-    ln ~/dotfiles/vim/vim ~/.config/nvim/vim --symbolic
+    ln ~/dotfiles/vim/lua ~/.config/nvim/lua --symbolic
 }
 
 echo "# Vim"
