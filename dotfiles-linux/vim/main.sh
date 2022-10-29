9fcd2a40995b2e28a0a2a7782727a8c9aeb02969
barts <bartsteensma@outlook.com>
2022-10-29 11:21:30 +0200

    nvim -> implemented after folder

--- dotfiles-linux/vim/main.sh
+++ dotfiles-linux/vim/main.sh
@@ -1,9 +1,14 @@
-sym_link_init_vim () {
+symlink_config () {
     mkdir ~/.config/nvim
     rm ~/.config/nvim/init.vim
+    rm ~/.config/nvim/after -r
+    rm ~/.config/nvim/vim -r
+
     ln ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim --symbolic
+    ln ~/dotfiles/vim/after ~/.config/nvim/after --symbolic
+    ln ~/dotfiles/vim/vim ~/.config/nvim/vim --symbolic
 }
 
 echo "# Vim"
-sym_link_init_vim 
+symlink_config 
 pip install pynvim
