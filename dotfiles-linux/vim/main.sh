beaa8143fbb6c3798d6802d58ae7e0b7507bdc1b
barts <bartsteensma@outlook.com>
2022-10-29 21:22:46 +0200

    renamed vim folder

--- dotfiles-linux/vim/main.sh
+++ dotfiles-linux/vim/main.sh
@@ -4,9 +4,9 @@ symlink_config () {
     rm ~/.config/nvim/after -r
     rm ~/.config/nvim/lua -r
 
-    ln ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim --symbolic
-    ln ~/dotfiles/vim/after ~/.config/nvim/after --symbolic
-    ln ~/dotfiles/vim/lua ~/.config/nvim/lua --symbolic
+    ln ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim --symbolic
+    ln ~/dotfiles/nvim/after ~/.config/nvim/after --symbolic
+    ln ~/dotfiles/nvim/lua ~/.config/nvim/lua --symbolic
 }
 
 echo "# Vim"
