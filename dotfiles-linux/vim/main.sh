65fb0c1dcf6c7d6fc4f680e4f31dfe01ed173135
barts <bartsteensma@outlook.com>
2022-11-02 09:59:47 +0100

    refactor vim

--- dotfiles-linux/vim/main.sh
+++ dotfiles-linux/vim/main.sh
@@ -3,10 +3,12 @@ symlink_config () {
     rm ~/.config/nvim/init.vim
     rm ~/.config/nvim/after -r
     rm ~/.config/nvim/lua -r
+    rm ~/.config/nvim/ftplugin -r
 
     ln ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim --symbolic
     ln ~/dotfiles/nvim/after ~/.config/nvim/after --symbolic
     ln ~/dotfiles/nvim/lua ~/.config/nvim/lua --symbolic
+    ln ~/dotfiles/nvim/ftplugin ~/.config/nvim/ftplugin --symbolic
 }
 
 echo "# Vim"
