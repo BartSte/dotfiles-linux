20d7d13be82bdbbc4d3af2f427a89ce99e291e20
barts <bartsteensma@outlook.com>
2022-11-06 18:46:40 +0100

    added waybar improvements

--- dotfiles-linux/dependencies/deps.sh
+++ dotfiles-linux/dependencies/deps.sh
@@ -1,5 +1,5 @@
 dependencies=("fzf" "python" "python-markdown" "powerline-fonts" "exa" "trash-cli" "unzip" "feh" "python-pip" "the_silver_searcher" "mutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd" "poppler" "antiword" "odt2txt" "tar" "p7zip" "fuse2" "fuse3" "fuse-common" "inetutils" "redshift" "base-devel" "neofetch" "ccls" "gdb" "make" "cmake" "wget" "htop" "man-db" "ripgrep" "xclip")
 dependencies_aur=("urlview" "kmonad-bin" "fzf-extras" "shadowfox-updater" "tulizu" "nvim-packer-git")
 dependencies_i3=("firefox" "firefox-tridactyl" "networkmanager" "rofi" "pavucontrol" "pulseaudio-alsa" "alsa-utils" "pulseaudio-bluetooth" "bluez-utils" "pulseaudio" "xorg" "xorg-xbacklight" "scrot" "polybar" "picom" "i3-wm" "lxsession")
-dependencies_sway=("brightnessctl")
+dependencies_sway=("brightnessctl", "waybar", "sway")
 
