242d8511365eef3f71c04499d7fc552c35d15e2b
barts <bartsteensma@outlook.com>
2022-12-10 14:47:31 +0100

    applied neovim settings to tmux

--- dotfiles-linux/bash/bindings.sh
+++ dotfiles-linux/bash/bindings.sh
@@ -1,10 +1,12 @@
 set -o vi
 
 bind -r "\C-t"
+bind -r "\C-r"
 bind '"\C-t":vi-movement-mode'
 bind '"\C-h":"\C-w"'
 bind -x '"\t": fzf_bash_completion'
 bind -x '"\C-l": clear;'
 bind -x '"\C-a": fzf_help;'
 bind -x '"\eo": fzf-file-widget'
+bind -x '"\eh": __fzf_history__'
 
