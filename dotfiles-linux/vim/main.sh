3d500949fe6a2659b0125435ffbe0eaeead41fd4
barts <bartsteensma@outlook.com>
2022-12-11 15:59:40 +0100

    added dictionary to vim

--- dotfiles-linux/vim/main.sh
+++ dotfiles-linux/vim/main.sh
@@ -1,5 +1,5 @@
 symlink_config () {
-    mkdir ~/.config/nvim
+    mkdir ~/.config/nvim --parents
     rm ~/.config/nvim/init.vim
     rm ~/.config/nvim/after -r
     rm ~/.config/nvim/lua -r
@@ -11,6 +11,22 @@ symlink_config () {
     ln ~/dotfiles/nvim/ftplugin ~/.config/nvim/ftplugin --symbolic
 }
 
+make_dict(){
+    lang=$1
+    storage=$2
+    file="$lang.dict"
+
+    echo "Creating aspell dictionary $file at $storage"
+    mkdir $storage --parents
+
+    pushd .
+    cd $storage
+    aspell -d $lang dump master | aspell -l $lang expand > $file
+    popd
+}
+
 echo "# Vim"
 symlink_config 
+make_dict nl "$HOME/.local/share/nvim/dict"
+make_dict en "$HOME/.local/share/nvim/dict"
 pip install pynvim
