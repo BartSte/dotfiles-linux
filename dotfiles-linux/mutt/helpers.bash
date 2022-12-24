5263777733f40c8b8651693b66e88f0709f62806
barts <bartsteensma@outlook.com>
2022-12-24 19:22:41 +0100

    added neomutt

--- dotfiles-linux/mutt/helpers.bash
+++ dotfiles-linux/mutt/helpers.bash
@@ -1,7 +1,6 @@
-install_icons() {
-    repo=https://github.com/sheoak/neomutt-powerline-nerdfonts.git
-    directory=$1
-
-    mkdir $directory
-    git clone $repo $directory/powerline
+install_theme() {
+    repo=$1
+    directory=$2
+    mkdir $directory --parents
+    git clone $repo $directory
 }
--- dotfiles-linux/mutt/main.sh
+++ dotfiles-linux/mutt/main.sh
@@ -1,4 +1,9 @@
 . ~/dotfiles-linux/mutt/helpers.bash
 
 echo "# Mutt"
-install_icons ~/.config/neomutt
+repo_gruvbox=https://git.sthu.org/repos/mutt-gruvbox.git 
+repo_powerline=https://github.com/sheoak/neomutt-powerline-nerdfonts.git
+directory=~/.config/neomutt
+
+install_theme $repo_gruvbox $directory/gruvbox
+install_theme $repo_powerline $directory/powerline
