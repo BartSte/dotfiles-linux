e8a2054d5d69bb50f3373748b77c5a8369134f44
barts <bartsteensma@outlook.com>
2023-02-26 17:21:13 +0100

    'added cpu and mem'

--- dotfiles-linux/dependencies/deps.sh
+++ dotfiles-linux/dependencies/deps.sh
@@ -1,6 +1,6 @@
 dependencies=("rust" "emacs" "khard" "vdirsyncer" "java-openjfx" "python-gpgme" "aspell" "aspell-nl" "openldap" "aspell-en" "urlscan" "texlive-bin" "texlive-core" "texlive-science" "gnuplot" "pandoc" "fzf" "python" "python-markdown" "powerline-fonts" "exa" "notmuch" "trash-cli" "unzip" "python-pip" "the_silver_searcher" "neomutt" "tmux" "bat" "lynx" "make" "alacritty" "tree" "fd" "poppler" "antiword" "odt2txt" "tar" "p7zip" "fuse2" "fuse3" "fuse-common" "inetutils" "redshift" "base-devel" "neofetch" "ccls" "gdb" "make" "cmake" "wget" "htop" "man-db" "ripgrep" "xclip" "wl-clipboard" "perl" "glow" "python")
 dependencies_i3=("spotifyd" "qutebrowser" "qt-wayland" "networkmanager" "pavucontrol" "pulseaudio-alsa" "alsa-utils" "pulseaudio-bluetooth" "bluez-utils" "pulseaudio" "xorg" "xorg-xbacklight" "scrot" "polybar" "picom" "i3-wm" "lxsession")
 dependencies_pip=("html2text" "python-vobject" "python-icalendar" "python-pytz" "python-tzlocal")
-dependencies_aur=("rmtrash" "oh-my-posh-bin" "diagon-git" "dropbox" "dropbox-cli" "urlview" "kmonad-bin" "shadowfox-updater" "tulizu" "nvim-packer-git", "sway-launcher-desktop" "gcalcli" "html2text-git" "python2")
+dependencies_aur=("rmtrash" "tmux-mem-cpu-load" "oh-my-posh-bin" "diagon-git" "dropbox" "dropbox-cli" "urlview" "kmonad-bin" "shadowfox-updater" "tulizu" "nvim-packer-git", "sway-launcher-desktop" "gcalcli" "html2text-git" "python2")
 dependencies_sway=("spotifyd" "brightnessctl" "waybar" "sway" "swaybg" "wayland" "gammastep")
 dependencies_cargo=("git-graph")
