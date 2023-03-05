1e4c62027e10decdf64278fcea95261f71dfc91b
barts <bartsteensma@outlook.com>
2023-03-05 14:22:40 +0100

    ''

--- dotfiles-linux/mutt/main.sh
+++ dotfiles-linux/mutt/main.sh
@@ -2,12 +2,14 @@ echo "# Neomutt"
 repo_gruvbox=https://git.sthu.org/repos/mutt-gruvbox.git 
 repo_powerline=https://github.com/sheoak/neomutt-powerline-nerdfonts.git
 directory=~/.config/neomutt
+cache="~/.cache/neomutt/$MUTT_ACCOUNT"
 
 mkdir $directory
-mkdir $directory/message_cache
-touch $directory/header_cache
 rm $directory/muttrc
 ln ~/dotfiles-linux/mutt/muttrc $directory/muttrc
+
+touch $cache/header_cache
+
 git clone $repo_gruvbox $directory/gruvbox
 git clone $repo_powerline $directory/powerline
 
