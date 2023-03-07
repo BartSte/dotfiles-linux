a9f747cb7bab4c50c7e40feca9e522f4692f305f
barts <b.steensma@fleetcleaner.com>
2023-03-07 08:12:55 +0100

    ''

--- dotfiles-linux/mutt/main.sh
+++ dotfiles-linux/mutt/main.sh
@@ -2,7 +2,7 @@ echo "# Neomutt"
 repo_gruvbox=https://git.sthu.org/repos/mutt-gruvbox.git 
 repo_powerline=https://github.com/sheoak/neomutt-powerline-nerdfonts.git
 directory=~/.config/neomutt
-cache="$HOME/.cache/neomutt/$MUTT_ACCOUNT"
+cache="$HOME/.cache/neomutt"
 
 mkdir $directory
 rm $directory/muttrc
@@ -15,4 +15,4 @@ git clone $repo_gruvbox $directory/gruvbox
 git clone $repo_powerline $directory/powerline
 
 echo "# Downloading emails"
-mbsync $MUTT_ACCOUNT
+mbsync -a
