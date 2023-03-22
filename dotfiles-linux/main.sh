55b3054189649a739337b414b51f1dec4a1e5c68
barts <b.steensma@fleetcleaner.com>
2023-03-22 11:03:39 +0100

    ''

--- dotfiles-linux/main.sh
+++ dotfiles-linux/main.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
 echo "Installing BartSte/dotfiles-linux repository"
 
-bash ~/dotfiles-linux/git/main.sh
-bash ~/dotfiles-linux/dependencies/main.sh
-bash ~/dotfiles-linux/wsl/main.sh
-bash ~/dotfiles-linux/fonts/main.sh
-bash ~/dotfiles-linux/vim/main.sh
-bash ~/dotfiles-linux/gpg/main.sh
-bash ~/dotfiles-linux/mutt/main.sh
-bash ~/dotfiles-linux/i3/main.sh
-bash ~/dotfiles-linux/kmonad/main.sh
-bash ~/dotfiles-linux/davmail/main.bash
-bash ~/dotfiles-linux/zsh/main.sh
+~/dotfiles-linux/git/main.sh
+~/dotfiles-linux/dependencies/main.sh
+~/dotfiles-linux/wsl/main.sh
+~/dotfiles-linux/fonts/main.sh
+~/dotfiles-linux/vim/main.sh
+~/dotfiles-linux/gpg/main.sh
+~/dotfiles-linux/mutt/main.sh
+~/dotfiles-linux/i3/main.sh
+~/dotfiles-linux/kmonad/main.sh
+~/dotfiles-linux/davmail/main.bash
+~/dotfiles-linux/zsh/main.sh
 
 echo "Done"
