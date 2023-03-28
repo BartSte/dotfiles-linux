f2793d056def759a29c56042496e9e6284f45450
barts <b.steensma@fleetcleaner.com>
2023-03-28 07:32:51 +0200

    ''

--- dotfiles-linux/sh/functions.sh
+++ dotfiles-linux/sh/functions.sh
@@ -6,18 +6,6 @@ del(){
     rmtrash $@
 }
 
-mymailsync() {
-    mailsync $HOME/.config/davmail/davmail.properties
-}
-
-mycalsync() {
-    running_wsl && dropbox=$WH/Dropbox || dropbox=$HOME/Dropbox
-    calsync \
-        $HOME/.config/davmail/davmail.properties \
-        $dropbox/org/outlook.org \
-        outlook_local today 180d 
-}
-
 fps() {
     ps aux | fzf
 }
