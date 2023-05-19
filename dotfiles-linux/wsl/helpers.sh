4d793b02132d46bfb7b7bf3a54b0c7678e20f86f
barts <b.steensma@fleetcleaner.com>
2023-05-19 22:47:27 +0200

    ''

--- dotfiles-linux/wsl/helpers.sh
+++ dotfiles-linux/wsl/helpers.sh
@@ -1,9 +1,3 @@
-enable_firefox_win() {
-    echo "Use windows's firefox for linux"
-    rm $HOME/bin/firefox
-    sudo ln --symbolic $HOME/dotfiles-linux/wsl/firefox_win $HOME/bin/firefox
-}
-
 enable_systemd() {
     echo "Enable systemd for WSL"
     sudo cp ~/dotfiles-linux/wsl/wsl.conf /etc/wsl.conf
--- dotfiles-linux/wsl/main.sh
+++ dotfiles-linux/wsl/main.sh
@@ -3,5 +3,4 @@
 . ~/dotfiles-linux/wsl/helpers.sh
 
 echo "# WSL"
-running_wsl && enable_firefox_win
 running_wsl && enable_systemd 
