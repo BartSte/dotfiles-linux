d6ca08ff4568ab60a95c6eb7bd55f5ab40aa0c86
barts <bartsteensma@outlook.com>
2023-06-15 21:09:14 +0200

    ''

--- dotfiles-linux/vim/main.sh
+++ dotfiles-linux/vim/main.sh
@@ -10,20 +10,15 @@ symlink_config () {
 
     dotfiles=~/dotfiles/nvim
     directory_config=~/.config/nvim
-    vimspector_home=~/.local/share/nvim/site/pack/packer/start/vimspector
 
     mkdir $directory_config --parents
     rm $directory_config/init.vim
-    rm $directory_config/after -r
     rm $directory_config/lua -r
     rm $directory_config/ftplugin -r
-    rm $vimspector_home/configurations
 
     ln $dotfiles/init.vim $directory_config/init.vim --symbolic
-    ln $dotfiles/after $directory_config/after --symbolic
     ln $dotfiles/lua $directory_config/lua --symbolic
     ln $dotfiles/ftplugin $directory_config/ftplugin --symbolic
-    ln $dotfiles/vimspector/configurations $vimspector_home/configurations --symbolic
 }
 
 make_dict(){
