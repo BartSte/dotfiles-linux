4caba3ca96c1e37d68d32ca32630c7fc7c5ced7c
barts <bartsteensma@outlook.com>
2023-07-02 17:15:39 +0200

    ''

--- dotfiles-linux/qutebrowser/helpers.sh
+++ dotfiles-linux/qutebrowser/helpers.sh
@@ -1,16 +1,26 @@
 link_config() {
     target=~/dotfiles/qutebrowser/config.py
     link=~/.config/qutebrowser/config.py
+
+    echo "Create link from $target to $link"
     ln -sf $target $link
 }
 
 # The bookmarks are private and shared across Dropbox.
 link_bookmarks() {
     if running_wsl; then
-        target="$WH/Dropbox/Config/urls"
+        target_urls="$WH/Dropbox/Config/urls"
+        target_quickmarks="$WH/Dropbox/Config/quickmarks"
     else
-        target=~/Dropbox/Config/urls
+        target_urls="$HOME/Dropbox/Config/urls"
+        target_quickmarks="$HOME/Dropbox/Config/quickmarks"
     fi
-    link=~/.config/qutebrowser/bookmarks/urls
-    ln -sf $target $link
+
+    link_urls="$HOME/.config/qutebrowser/bookmarks/urls"
+    link_quickmarks="$HOME/.config/qutebrowser/quickmarks"
+
+    echo "Create links from $target_urls to $link_urls"
+    echo "Create links from $target_quickmarks to $link_quickmarks"
+    ln -sf $target_urls $link_urls
+    ln -sf $target_quickmarks $link_quickmarks
 }
