cb07bdf0e19ddb5799ecef0f492da97f5acee534
barts <b.steensma@fleetcleaner.com>
2023-07-07 22:40:16 +0200

    ''

--- dotfiles-linux/zsh/aliases.zsh
+++ dotfiles-linux/zsh/aliases.zsh
@@ -3,21 +3,23 @@ alias ..='cd ..'
 alias ...='cd ./../../'
 alias ....='cd ./../../../'
 
-alias pdf="zathura $@"
 alias py='python3'
 alias ta='tmux attach'
 alias tm='tmux-session ~'
-alias ts="tmux-session $@"
 alias ls='exa -h --icons'
+alias ts="tmux-session $@"
 alias ll='exa -hl --icons --git'
 alias la='exa -hal --icons --git' 
 alias vim='nvim $@'
+alias pdf="zathura $@"
 alias lsa='exa -ah --icons'
 alias bat='bat --theme gruvbox-dark'
 alias mutt='neomutt'
 alias dact='deactivate'
 alias path='echo -e ${PATH//:/\\n}'
 alias reload='source ~/.zshrc'
+alias summary='gtm report -all=true -full-message=false -this-week=true -format=summary'
+alias timeline='gtm report -all=true -full-message=false -this-week=true -format=timeline-hours'
 alias blueon='bluetoothctl power on; bluetoothctl discoverable on; bluetoothctl pairable on;'
 alias blueoff='bluetoothctl power off'
 alias earbuds='bluetoothctl connect $EARBUDS'
