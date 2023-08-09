f200817261d09feefc597bdfd284bf1a616d190a
barts <bartsteensma@outlook.com>
2023-08-09 18:30:17 +0200

    ''

--- dotfiles-linux/dependencies/helpers.sh
+++ dotfiles-linux/dependencies/helpers.sh
@@ -1,3 +1,11 @@
+dir=~/dotfiles-linux/dependencies
+
+install_python_packages () {
+    echo "Install python packages"
+    pip install pipx
+    xargs -n 1 pipx install < $1
+}
+
 install_node_js () {
     echo "Install node.js"
     sudo bash -c "$(curl -sL install-node.vercel.app/lts)" -y -f
