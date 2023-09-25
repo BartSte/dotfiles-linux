b9efd9f491c0e7f24e3efe4fef7c7da44c82a73d
barts <b.steensma@fleetcleaner.com>
2023-09-25 17:24:23 +0200

    ''

--- dotfiles-linux/zsh/aliases.zsh
+++ dotfiles-linux/zsh/aliases.zsh
@@ -22,6 +22,9 @@ alias ta='tmux attach'
 alias tm='tmux-session ~'
 alias ts="tmux-session $@"
 alias vim='nvim $@'
+alias wpip='wpy -m pip $@'
+alias wpytest='wpy -m pytest $@'
+
 
 alias graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
