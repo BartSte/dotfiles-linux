1a8f5e620b737a80e1922bba6a98abcf1f2be47b
barts <b.steensma@fleetcleaner.com>
2023-09-29 09:35:32 +0200

    ''

--- dotfiles-linux/dependencies/helpers.sh
+++ dotfiles-linux/dependencies/helpers.sh
@@ -17,3 +17,18 @@ install_yay () {
     makepkg -si --noconfirm
     ~
 }
+
+# See BarsSte/tmux-session
+install-tmux-session () {
+    echo "Install tmux-session"
+    local tmp_dir=$(mktemp -d);
+    git clone https://github.com/BartSte/tmux-session.git $tmp_dir;
+    sudo $tmp_dir/install;
+    rm -rf $tmp_dir;
+}
+
+# See BarsSte/fzf-help
+install_fzf_help(){
+    bash -c 'tmp_dir=$(mktemp -d); git clone https://github.com/BartSte/fzf-help.git $tmp_dir; $tmp_dir/install; rm -rf $tmp_dir;'
+}
+
