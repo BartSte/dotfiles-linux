590501af801982efe69d15b1e5095959da62f5c1
barts <bartsteensma@outlook.com>
2024-01-13 20:50:59 +0100

    ''

--- dotfiles-linux/tmux/snapshotrc.zsh
+++ dotfiles-linux/tmux/snapshotrc.zsh
@@ -6,4 +6,5 @@ export CMAKE_COLOR_DIAGNOSTICS="ON"
 export CMAKE_GENERATOR="Ninja"
 
 alias unsetc='unset CC; unset CXX; unset CXXFLAGS; echo "CC, CXX, CXXFLAGS unset"'
+alias init='cmake -G Ninja -S . -B ./build -Wdev'
 alias compile='cmake --build ./build --target package'
