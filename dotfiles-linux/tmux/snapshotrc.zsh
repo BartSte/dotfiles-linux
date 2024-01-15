5280f7ebe09a9bc16496411df567cce03c986f0c
barts <bartsteensma@outlook.com>
2024-01-15 21:28:19 +0100

    ''

--- dotfiles-linux/tmux/snapshotrc.zsh
+++ dotfiles-linux/tmux/snapshotrc.zsh
@@ -6,5 +6,5 @@ export CMAKE_COLOR_DIAGNOSTICS="ON"
 export CMAKE_GENERATOR="Ninja"
 
 alias unsetc='unset CC; unset CXX; unset CXXFLAGS; echo "CC, CXX, CXXFLAGS unset"'
-alias init='cmake -G Ninja -S . -B ./build -Wdev'
+alias init='cmake -G Ninja -S . -B ./build -Wdev -DPC_FFMPEG_LIBRARY_DIRS=./3rdparty/ffmpeg/lib -DPC_FFMPEG_INCLUDE_DIRS=./3rdparty/ffmpeg/include'
 alias compile='cmake --build ./build --target package'
