792b91bb9316bfbf126e6b16a94e414d6219566c
barts <bartsteensma@outlook.com>
2024-01-21 21:29:09 +0100

    ''

--- dotfiles-linux/tmux/snapshotrc.zsh
+++ dotfiles-linux/tmux/snapshotrc.zsh
@@ -1,10 +1,10 @@
 export CC="$(which clang)"
 export CXX="$(which clang++)"
-export CXXFLAGS="-Wall -fdiagnostics-color=always"
+export CXXFLAGS="-Wall -Wextra -Wno-unused-parameter -fdiagnostics-color=always"
 export CMAKE_EXPORT_COMPILE_COMMANDS="ON"
 export CMAKE_COLOR_DIAGNOSTICS="ON"
 export CMAKE_GENERATOR="Ninja"
 
 alias unsetc='unset CC; unset CXX; unset CXXFLAGS; echo "CC, CXX, CXXFLAGS unset"'
-alias init='cmake -G Ninja -S . -B ./build -Wdev -DPC_FFMPEG_LIBRARY_DIRS=./3rdparty/ffmpeg/lib -DPC_FFMPEG_INCLUDE_DIRS=./3rdparty/ffmpeg/include'
-alias compile='cmake --build ./build --target package'
+alias compile='cmake --build "~/code/snapshot/build" --target package'
+alias test='ctest --test-dir "~/code/snapshot/build" --verbose'
