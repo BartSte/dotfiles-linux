d9ac1df02959a39da8326531f2c6fd69a04f00c7
barts <b.steensma@fleetcleaner.com>
2024-02-05 18:19:18 +0100

    ''

--- dotfiles-linux/zsh/aliases.zsh
+++ dotfiles-linux/zsh/aliases.zsh
@@ -27,6 +27,3 @@ alias vim='nvim $@'
 alias wpip='wpy -m pip $@'
 alias wpytest='wpy -m pytest $@'
 alias wipdb='wpy -m ipdb $@'
-
-alias graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-
