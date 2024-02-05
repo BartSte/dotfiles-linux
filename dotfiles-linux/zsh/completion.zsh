cbf17561adbf8c146112dfba3a428d2481bdee4b
barts <b.steensma@fleetcleaner.com>
2024-02-05 17:50:56 +0100

    ''

--- dotfiles-linux/zsh/completion.zsh
+++ dotfiles-linux/zsh/completion.zsh
@@ -1,11 +1,6 @@
-save_source() {
-    local file=$1
-    if [ -f "$file" ]; then
-        source $file
-    fi
-}
-
-zstyle :compinstall filename '~/.zshrc'
-autoload -Uz compinit
-compinit
-save_source /usr/share/zsh/plugins/fzf-tab-git/fzf-tab.zsh
+if [[ -f /usr/share/zsh/plugins/fzf-tab-git/fzf-tab.zsh ]]; then
+    zstyle :compinstall filename '~/.zshrc'
+    autoload -Uz compinit
+    compinit
+    save_source /usr/share/zsh/plugins/fzf-tab-git/fzf-tab.zsh
+fi
