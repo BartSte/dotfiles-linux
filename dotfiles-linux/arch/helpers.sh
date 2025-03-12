f12365f84ca5a73148109868606828b618300674
barts <bartsteensma@outlook.com>
2025-03-12 13:44:21 +0100

    'Automatic Update'

--- dotfiles-linux/arch/helpers.sh
+++ dotfiles-linux/arch/helpers.sh
@@ -1,9 +1,3 @@
-install_python_packages() {
-    echo "Install python packages"
-    local deps_file="$1"
-    xargs -n 1 pipx install <"$deps_file"
-}
-
 install_node_js() {
     echo "Install node.js"
     sudo bash -c "$(curl -sL install-node.vercel.app/lts)" -y -f
@@ -14,7 +8,8 @@ install_yay() {
     git clone https://aur.archlinux.org/yay.git "$dir"
     old_dir=$(pwd)
     cd "$dir" || exit
-    makepkg -si --noconfirm 
+    makepkg -s --noconfirm
+    sudo pacman -U --noconfirm *.pkg.tar.*
     cd "$old_dir" || exit
     rm -rf "$dir"
 }
