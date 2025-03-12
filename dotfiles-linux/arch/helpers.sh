014a7e662859d9eda78d3a03572ebbfecae62a97
barts <bartsteensma@outlook.com>
2025-03-12 13:09:01 +0100

    'Automatic Update'

--- dotfiles-linux/arch/helpers.sh
+++ dotfiles-linux/arch/helpers.sh
@@ -14,7 +14,8 @@ install_yay() {
     git clone https://aur.archlinux.org/yay.git "$dir"
     old_dir=$(pwd)
     cd "$dir" || exit
-    makepkg -si --noconfirm
+    makepkg -s --noconfirm
+    sudo pacman -U --noconfirm *.pkg.tar.*
     cd "$old_dir" || exit
     rm -rf "$dir"
 }
