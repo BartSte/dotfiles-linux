500eb29df3458b56efa208c2c32895d2af39ad6c
barts <b.steensma@fleetcleaner.com>
2025-04-03 08:50:22 +0200

    'Automatic Update'

--- dotfiles-linux/zsh/projectrc/projects/fr_pyproject.zsh
+++ dotfiles-linux/zsh/projectrc/projects/fr_pyproject.zsh
@@ -3,14 +3,6 @@
 this_dir=$(readlink -f "$(dirname "$0")")
 source $this_dir/pyprojects.zsh
 
-# Use this configuration when you are doing a python project for windows. This
-# way you can work in wsl and call to windows executables when running code.
-<<<<<<< HEAD:dotfiles-linux/zsh/projectrc/projects/fr_pyproject.zsh
-=======
-prompts="$HOME/dotfiles-linux/aider/prompts"
-export AIDER_READ="[$dir/conventions.md, $dir/python/conventions.md]"
-
->>>>>>> 6a88397b3271b0bda35e667efebef63fee8d456a:dotfiles-linux/zsh/projectrc/projects/winpyproject.zsh
 if is_running wsl; then
     name="$(basename $(pwd))"
     export WINVENV=$WH/venvs/$name
