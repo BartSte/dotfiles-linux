3e8927f31122d19871d0ff3bb3a4cb71d8f64fdd
barts <bartsteensma@outlook.com>
2025-04-05 12:47:18 +0200

    'Automatic Update'

--- dotfiles-linux/zsh/aliases.zsh
+++ dotfiles-linux/zsh/aliases.zsh
@@ -5,7 +5,6 @@ alias ....='cd ./../../../'
 alias .....='cd ./../../../../'
 alias ......='cd ./../../../../../'
 
-alias ai="source $HOME/dotfiles-linux/aider/host_settings.zsh; aider"
 alias prompts="source $HOME/dotfiles-linux/aider/host_settings.zsh; prompts"
 alias act='source .venv/bin/activate'
 alias bat='bat --theme gruvbox-dark'
