727509eaa0c2c2cc56ec05cd532e59cd9fb603bc
barts <b.steensma@fleetcleaner.com>
2025-04-09 18:51:46 +0200

    'Automatic Update'

--- dotfiles-linux/zsh/functions.zsh
+++ dotfiles-linux/zsh/functions.zsh
@@ -28,23 +28,62 @@ fullsync() {
 }
 
 ###############################################################################
-# Run aider
+# Run aider chat
 # Before, specify host and project settings are loaded.
 ###############################################################################
-ai() {
+chat() {
     local dir
     dir=$HOME/dotfiles-linux/aider
-    if [[ -f $dir/host_settings.zsh ]]; then
-        source $dir/host_settings.zsh
-    fi
-    if [[ -f $dir/$PROJECTRC.zsh ]]; then
-        source $dir/$PROJECTRC.zsh
+    save-source $dir/host_settings.zsh
+    save-source $dir/$PROJECTRC.zsh
+    aider
+}
+
+###############################################################################
+# Run aider --message with a prompts from bartste-prompts
+###############################################################################
+ai() {
+    usage="Usage: ai [options] command file1 file2 ...
+
+    Runs 'aider --yes-always --message <message> file1 file2 ...' where
+    <message> is retrieved from bartste-prompts.
+
+    Options:
+    -h, --help      Show this help message.
+    -f, --filetype  Specify a file type for the prompt.
+    -q, --quiet     Disable output to stderr."""
+
+    local prompt file_type quiet command files message
+    while (("$#")); do
+        case $1 in
+        -h | --help)
+            echo "$usage"
+            return
+            ;;
+        -f | --filetype)
+            file_type=$2
+            shift 2
+            ;;
+        -q | --quiet)
+            quiet="--quiet"
+            shift
+            ;;
+        *)
+            if [[ -z "$command" ]]; then
+                command=$1
+            else
+                files+=("$1")
+            fi
+            shift
+            ;;
+        esac
+    done
+
+    if [[ -z "$command" ]]; then
+        echo "Error: No command specified."
+        echo "$usage"
+        return 1
     fi
 
-    # If no args, run aider solo
-    if [[ $# -eq 0 ]]; then
-        aider
-    else # Otherwise, run aider --message $(prompts <options> <command>) [files]
-        # TODO
-    fi
+    # TODO
 }
