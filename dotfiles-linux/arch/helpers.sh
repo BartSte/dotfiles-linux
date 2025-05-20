5ab919e2ae59f8a32565aff14f6577355808b45c
barts <b.steensma@fleetcleaner.com>
2025-05-20 14:16:24 +0200

    'Automatic Update'

--- dotfiles-linux/arch/helpers.sh
+++ dotfiles-linux/arch/helpers.sh
@@ -1,8 +1,3 @@
-install_node_js() {
-    echo "Install node.js"
-    sudo bash -c "$(curl -sL install-node.vercel.app/lts)" -y -f
-}
-
 install_yay() {
     local dir="$1"
     git clone https://aur.archlinux.org/yay.git "$dir"
