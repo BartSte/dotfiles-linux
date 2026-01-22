595e118cd3fdc9b85a182c5dae71ba03962f6e69
bartste <b.steensma@fleetrobotics.com>
2026-01-22 08:46:50 +0100

    'Automatic Update'

--- dotfiles-linux/zsh/completion.zsh
+++ dotfiles-linux/zsh/completion.zsh
@@ -1,5 +1,6 @@
+fpath=(~/dotfiles-linux/zsh/completions $fpath)
+
 if [[ -f /usr/share/zsh/plugins/fzf-tab-git/fzf-tab.zsh ]]; then
-    zstyle :compinstall filename '~/.zshrc'
     autoload -Uz compinit
     compinit
     save-source /usr/share/zsh/plugins/fzf-tab-git/fzf-tab.zsh
