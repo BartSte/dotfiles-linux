e186181c4d5ff887eb4e67950ba941dd97785e2f
BartSte <>
2026-01-30 16:47:55 +0000

    Split arch/pi modules into separate repos

--- dotfiles-linux/arch/helpers.sh
+++
@@ -1,9 +0,0 @@
-install_yay() {
-    local dir="$1"
-    git clone https://aur.archlinux.org/yay.git "$dir"
-    old_dir=$(pwd)
-    cd "$dir" || exit
-    makepkg -si --noconfirm
-    cd "$old_dir" || exit
-    rm -rf "$dir"
-}
--- dotfiles-linux/moltbot/README.md
+++
@@ -1,6 +0,0 @@
-# moltbot
-
-Custom moltbot setup for Raspberry Pi.
-
-⚠️ **Do not store secrets here.** This repo is public.
-Store private data in rbw and load it at runtime.
