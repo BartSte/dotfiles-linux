2a99032ccaa4c2bb03c5a0c072d6478d3aef055a
BartSte <bartsteensma@outlook.com>
2026-03-25 18:11:51 +0100

    'Automatic Update'

--- dotfiles-linux/zsh/aliases.zsh
+++ dotfiles-linux/zsh/aliases.zsh
@@ -11,7 +11,7 @@ alias blueoff='bluetoothctl power off'
 alias blueon='bluetoothctl power on; bluetoothctl discoverable on; bluetoothctl pairable on;'
 alias chrome='google-chrome-stable --ozone-platform=wayland'
 alias dact='deactivate'
-alias dropsync='rclone bisync dropbox: ~/dropbox --remove-empty-dirs --progress'
+alias dropsync='rclone bisync dropbox: ~/dropbox --remove-empty-dirs --progress --conflict-resolve newer'
 alias dsize='du -h -d1' # use the du command for further inspection
 alias earbuds='bluetoothctl connect $EARBUDS'
 alias fps="ps aux | fzf"
